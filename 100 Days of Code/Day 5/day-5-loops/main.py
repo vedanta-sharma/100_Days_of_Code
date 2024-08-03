5d208a6d9cb7aa134869fc89578cd81f5e3f16d8
vedanta-sharma <126336689+vedanta-sharma@users.noreply.github.com>
2024-08-03 23:17:28 +0530

    Update main.py

--- 100 Days of Code/Day 5/day-5-loops/main.py
+++ 100 Days of Code/Day 5/day-5-loops/main.py
@@ -1,12 +1,3 @@
-#DAY - 1
-#100DaysofCode
-# Band Name Generator
-
-# print("Welcome to the Band Name Generator!")
-# user_city = input("What's the name of the city you grew up in?\n")
-# user_pet = input("What's tne name of your pet?\n")
-# print("Your band name could be: " + user_city + " " + user_pet)
-
 #Day 5
 #100DaysofCode
 # Average Height with loops
