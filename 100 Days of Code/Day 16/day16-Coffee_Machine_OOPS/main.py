75b18b0c5d01b3393d31189fc52132fa20910d03
vedanta-sharma <126336689+vedanta-sharma@users.noreply.github.com>
2024-08-16 11:20:46 +0530

    Add files via upload

---
+++ 100 Days of Code/Day 16/day16-Coffee_Machine_OOPS/main.py
@@ -0,0 +1,22 @@
+#Day 16
+#Coffee Machine using OOPS
+from Menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_macine import MoneyMachine
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+is_on = True
+while is_on:
+  options = menu.get_items()
+  choice = input(f"What would you like? ({options}): ")
+  if choice == "off":
+    is_on = False
+  elif choice == "report":
+    coffee_maker.report()
+    money_machine.report()
+  else:
+    drink = menu.find_drink(choice)
+    if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+      coffee_maker.make_coffee(drink)
